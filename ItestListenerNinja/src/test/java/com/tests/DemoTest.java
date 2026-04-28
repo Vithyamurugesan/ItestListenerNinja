ae5c99147cfb09cc9c481047d12ee3a5272171dc
Vithyamurugesan <vithyabtech54@gmail.com>
2026-04-28 10:39:49 +0530

    Log updated

--- ItestListenerNinja/src/test/java/com/tests/DemoTest.java
+++ ItestListenerNinja/src/test/java/com/tests/DemoTest.java
@@ -9,35 +9,61 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @Listeners(DemoListenerFail.class)
 public class DemoTest {
 
-        public WebDriver driver;
-       
-	    @BeforeMethod
-	    public void beforeTest() {
-	        driver = new ChromeDriver();
-	        driver.manage().window().maximize();
-	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	        driver.get("https://tutorialsninja.com/demo/index.php");
-	    }
-	   
-	    @Test
-	    public void loginTest() {
-	        driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
-	        driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
-	        driver.findElement(By.id("input-email")).sendKeys("jennysenthil123@gmail.com");
-	        driver.findElement(By.id("input-password")).sendKeys("jenny");
-	        driver.findElement(By.xpath("//input[@value='Login']")).click();
+    public WebDriver driver;
 
-	        String title = driver.getTitle();
-	        Assert.assertTrue(driver.getPageSource().contains("My Account"));
-	    }
 
-	    @AfterMethod
-	    public void afterTest() {
-	        if (driver != null) {
-	            driver.quit();
-	        }
-	    }
-	}
+    private static final Logger log = LogManager.getLogger(DemoTest.class);
+
+    @BeforeMethod
+    public void beforeTest() {
+        log.info("Launching browser");
+        driver = new ChromeDriver();
+
+        log.info("Maximizing window");
+        driver.manage().window().maximize();
+
+        log.info("Setting implicit wait");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        log.info("Opening application URL");
+        driver.get("https://tutorialsninja.com/demo/index.php");
+    }
+
+    @Test
+    public void loginTest() {
+        log.info("Clicking My Account");
+        driver.findElement(By.xpath("//span[normalize-space()='My Account']")).click();
+
+        log.info("Clicking Login");
+        driver.findElement(By.xpath("//a[normalize-space()='Login']")).click();
+
+        log.info("Entering email");
+        driver.findElement(By.id("input-email")).sendKeys("jennysenthil123@gmail.com");
+
+        log.info("Entering password");
+        driver.findElement(By.id("input-password")).sendKeys("jenny");
+
+        log.info("Clicking Login button");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        log.info("Validating login result");
+        Assert.assertTrue(driver.getPageSource().contains("My Account"));
+
+        log.info("Login test completed successfully");
+    }
+
+    @AfterMethod
+    public void afterTest() {
+        log.info("Closing browser");
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
